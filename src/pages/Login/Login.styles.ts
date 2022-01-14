90bc2fe83b5768a5fc69848b2861959d4b8ca320
JoseBieco <jo_bieco@hotmail.com>
2022-01-14 10:22:38 -0300

    feat(src/paages/Login/Login.styles.ts): background image, root size and login form container style

---
+++ src/pages/Login/Login.styles.ts
@@ -0,0 +1,21 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+import Image from '../../assets/images/full_bookshelf.jpg';
+
+export const loginStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh'
+  },
+  image: {
+    backgroundImage: `url(${Image})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  },
+  paper: {
+    margin: theme.spacing('50%', 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
+}));
--- src/pages/Login/login.styles.ts
+++
@@ -1,7 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles';
-
-export const loginStyles = makeStyles((theme) => ({
-  root: {
-    color: 'red'
-  }
-}));
