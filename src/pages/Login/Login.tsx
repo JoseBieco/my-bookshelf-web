6e84e0df67f0c935e7f827656014d102db4b1ac3
JoseBieco <jo_bieco@hotmail.com>
2022-01-14 10:20:38 -0300

    feat(src/pages/Login.tsx): set page grid

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
+import { Grid, Paper, Typography } from '@material-ui/core';
+
+import LoginForm from '../../components/LoginForm/LoginForm';
+
 import { loginStyles } from './login.styles';
 
 const Login: React.FC = () => {
   const classes = loginStyles();
-  return <div className={classes.root}>Login</div>;
+  return (
+    <Grid container className={classes.root}>
+      <Grid item xs={false} sm={4} md={9} className={classes.image} />
+      <Grid item xs={12} sm={8} md={3} component={Paper} elevation={8}>
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <LoginForm />
+        </div>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Login;
