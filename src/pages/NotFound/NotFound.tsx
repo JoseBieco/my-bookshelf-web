8da230d21166be723205e2fceaf4433c266ff263
JoseBieco <jo_bieco@hotmail.com>
2022-01-14 14:51:16 -0300

    feat(src/pages/NotFound/NotFound.tsx): basic page template

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
+import { Grid, Typography } from '@material-ui/core';
+
+import { notFoundStyles } from './NotFound.styles';
+
 const NotFound: React.FC = () => {
+  const classes = notFoundStyles();
   return (
-    <div>
-      404 - não achei
-    </div>
+    <Grid container className={classes.root}>
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        md={12}
+        className={`${classes.image} ${classes.flexContent}`}
+      >
+        <Typography component="h1" variant="h5">
+          404 - não achei
+        </Typography>
+      </Grid>
+    </Grid>
   );
-}
+};
 
 export default NotFound;
