215358631da40199fbf9180ba6ab04dc13320492
JoseBieco <jo_bieco@hotmail.com>
2022-01-14 10:24:32 -0300

    feat(src/components/LoginForm/LoginForm.styles.ts): login form styles

---
+++ src/components/LoginForm/LoginForm.styles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const loginFromStyles = makeStyles((theme) => ({
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
