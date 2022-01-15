40cff1b23f94ac141a5dcc51d30a85b8b3943b95
JoseBieco <jo_bieco@hotmail.com>
2022-01-15 18:26:14 -0300

    feat(src/components/LoginForm/LoginForm.styles.ts): set height to form

--- src/components/LoginForm/LoginForm.styles.ts
+++ src/components/LoginForm/LoginForm.styles.ts
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export const loginFromStyles = makeStyles((theme) => ({
   form: {
     width: '100%',
+    height: '90%',
     marginTop: theme.spacing(1)
   },
   submit: {
