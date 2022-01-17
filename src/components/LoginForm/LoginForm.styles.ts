a02338a1cfb12131b04a1025c0e525c4326e2bc4
JoseBieco <jo_bieco@hotmail.com>
2022-01-17 08:42:11 -0300

    style(src/components/LoginForm/LoginForm.styles.ts): changed color of submit button and, input field border and label colors

--- src/components/LoginForm/LoginForm.styles.ts
+++ src/components/LoginForm/LoginForm.styles.ts
@@ -4,9 +4,21 @@ export const loginFromStyles = makeStyles((theme) => ({
   form: {
     width: '100%',
     height: '90%',
-    marginTop: theme.spacing(1)
+    marginTop: theme.spacing(1),
+    '& label.Mui-focused': {
+      color: '#F23405'
+    },
+    '& .MuiOutlinedInput-root': {
+      '&.Mui-focused fieldset': {
+        borderColor: '#F23405'
+      }
+    }
   },
   submit: {
-    margin: theme.spacing(3, 0, 2)
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: '#F23405',
+    '&:hover': {
+      backgroundColor: '#F5481D'
+    }
   }
 }));
