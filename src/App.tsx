4f166dd6f13e7f1a9a4c30e606c99660bd680f08
JoseBieco <jo_bieco@hotmail.com>
2022-01-20 10:52:05 -0300

    feat(src/App.tsx): add AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-
 import './App.css';
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { AuthProvider } from './context/AuthContext';
 import Login from './pages/Login/Login';
 import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
